2919fb4f4f7786ca5e6e5f1ea7a000e1d0d1f1d7
oluwatobi Adeenkan <tadenekan@dlabs.cloud>
2020-09-10 00:53:01 +0100

    quick-fix: Removed generics from find

--- src/contracts/early-access-repository.interface.ts
+++ src/contracts/early-access-repository.interface.ts
@@ -1,10 +1,10 @@
 export interface EarlyAccessRepository {
-  create(email: string, name?: string): Promise<boolean>;
+    create(email: string, name?: string): Promise<boolean>;
 
-  delete(email: string): Promise<boolean>;
+    delete(email: string): Promise<boolean>;
 
 
-  find<T>(email: string): Promise<T> | Promise<null>;
+    find(email: string): Promise<unknown> | Promise<null>;
 }
 
 export const EarlyAccessRepository = Symbol('EarlyAccessRepository');
