b29d59e33a987e7cb6d207255a6e22cc4060850b
oluwatobi Adeenkan <tadenekan@dlabs.cloud>
2020-09-11 09:31:05 +0100

    Removed assets from controller

--- src/controllers/early-access.controller.ts
+++ src/controllers/early-access.controller.ts
@@ -1,70 +1,68 @@
-import { EarlyAccessService } from '../contracts/early-access-service.interface';
-import { SubscribeDto } from '../dtos/subcribe.dto';
-import { ConfigImpl } from '../impl/config.impl';
-import { ViewCompilerImpl } from '../impl/view-compiler.impl';
-import { Body, Controller, Delete, Get, Inject, Param, Post, Req, Res } from '@nestjs/common';
-import { Request, Response } from 'express';
+import {EarlyAccessService} from '../contracts/early-access-service.interface';
+import {SubscribeDto} from '../dtos/subcribe.dto';
+import {ConfigImpl} from '../impl/config.impl';
+import {ViewCompilerImpl} from '../impl/view-compiler.impl';
+import {Body, Controller, Delete, Get, Inject, Param, Post, Req, Res} from '@nestjs/common';
+import {Request, Response} from 'express';
 
 @Controller('early-access')
 export class EarlyAccessController {
 
-  constructor(@Inject(EarlyAccessService) private readonly earlyAccessService: EarlyAccessService,
-              private readonly configService: ConfigImpl) {
+    constructor(@Inject(EarlyAccessService) private readonly earlyAccessService: EarlyAccessService,
+                private readonly configService: ConfigImpl) {
 
-  }
-
-  @Get()
-  public async index(@Res() res: Response) {
-
-    let assetsDir = this.configService.viewTemplate()?.assetsDir ? this.configService.viewTemplate()?.assetsDir : `${__dirname}/../assets`;
-    let viewsDir = this.configService.viewTemplate()?.viewDir ? `${this.configService.viewTemplate()?.viewDir}` : `${__dirname}/../views`;
-    let data = {
-      showShareWithTwitter: !!this.configService.getTwitterHandle(),
-      assetsDir,
-    };
-    
-    let indexPath = this.configService.viewTemplate()?.index ? `${viewsDir}/${this.configService.viewTemplate()?.index}` : `${viewsDir}/index.ejs`;
-
-    let view = new ViewCompilerImpl().compileView(indexPath, data);
-    return res.send(view);
-  }
-
-  @Post()
-  public async subscribe(@Body() subscribeDto: SubscribeDto, @Req() request: Request, @Res() res: Response) {
-    this.earlyAccessService.subscribe(subscribeDto.email, subscribeDto.name)
-      .then(isSubscribed => {
-        let url = `${request.baseUrl}/early-access?isSubscribed=${isSubscribed}`;
-        res.redirect(url);
-      }).catch(error => {
-      let url = `${request.baseUrl}/early-access?isSubscribed=${false}`;
-      res.redirect(url);
-    });
-  }
-
-  @Delete('/:email')
-  public unSubscribe(@Param('email') email: string, @Req() req: Request, @Res() res: Response) {
-    this.earlyAccessService.unSubscribe(email).then(unSubscribe => {
-      let url = `${req.baseUrl}/early-access?unSubscribe=${unSubscribe}`;
-      res.redirect(url);
-    }).catch(error => {
-      let url = `${req.baseUrl}/early-access?unSubscribe=${false}`;
-      res.redirect(url);
-    });
-  }
-
-  @Get('/twitter-share')
-  public shareOnTwitter(@Req() request: Request, @Res() response: Response) {
-    let twitterHandle = this.configService.getTwitterHandle();
-    let url = `${request.baseUrl}/early-access`;
-    if (twitterHandle) {
-      let twitterShareMessage = this.configService.twitterShareMessage();
-      url = twitterShareMessage ?? `@${twitterHandle}is coming soon. Request early access to be one of the first people to try it out ${url}`;
-      let redirectURL = `https://twitter.com/intent/tweet?text=${encodeURIComponent(url)}&related=${twitterHandle}&handle=${twitterHandle}`;
-      response.redirect(redirectURL);
-      return;
     }
 
-    response.redirect(url);
+    @Get()
+    public async index(@Res() res: Response) {
 
-  }
+        let viewsDir = this.configService.viewTemplate()?.viewDir ? `${this.configService.viewTemplate()?.viewDir}` : `${__dirname}/../views`;
+        let data = {
+            showShareWithTwitter: !!this.configService.getTwitterHandle()
+        };
+
+        let indexPath = this.configService.viewTemplate()?.index ? `${viewsDir}/${this.configService.viewTemplate()?.index}` : `${viewsDir}/index.ejs`;
+
+        let view = new ViewCompilerImpl().compileView(indexPath, data);
+        return res.send(view);
+    }
+
+    @Post()
+    public async subscribe(@Body() subscribeDto: SubscribeDto, @Req() request: Request, @Res() res: Response) {
+        this.earlyAccessService.subscribe(subscribeDto.email, subscribeDto.name)
+            .then(isSubscribed => {
+                let url = `${request.baseUrl}/early-access?isSubscribed=${isSubscribed}`;
+                res.redirect(url);
+            }).catch(error => {
+            let url = `${request.baseUrl}/early-access?isSubscribed=${false}`;
+            res.redirect(url);
+        });
+    }
+
+    @Delete('/:email')
+    public unSubscribe(@Param('email') email: string, @Req() req: Request, @Res() res: Response) {
+        this.earlyAccessService.unSubscribe(email).then(unSubscribe => {
+            let url = `${req.baseUrl}/early-access?unSubscribe=${unSubscribe}`;
+            res.redirect(url);
+        }).catch(error => {
+            let url = `${req.baseUrl}/early-access?unSubscribe=${false}`;
+            res.redirect(url);
+        });
+    }
+
+    @Get('/twitter-share')
+    public shareOnTwitter(@Req() request: Request, @Res() response: Response) {
+        let twitterHandle = this.configService.getTwitterHandle();
+        let url = `${request.baseUrl}/early-access`;
+        if (twitterHandle) {
+            let twitterShareMessage = this.configService.twitterShareMessage();
+            url = twitterShareMessage ?? `@${twitterHandle}is coming soon. Request early access to be one of the first people to try it out ${url}`;
+            let redirectURL = `https://twitter.com/intent/tweet?text=${encodeURIComponent(url)}&related=${twitterHandle}&handle=${twitterHandle}`;
+            response.redirect(redirectURL);
+            return;
+        }
+
+        response.redirect(url);
+
+    }
 }
