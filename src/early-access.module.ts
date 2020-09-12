4709ae14de58b6f4d9e52b8f29423e804be66cac
oluwatobi Adeenkan <tadenekan@dlabs.cloud>
2020-09-12 10:47:04 +0100

    Added directory validation

--- src/early-access.module.ts
+++ src/early-access.module.ts
@@ -1,67 +1,67 @@
-import { DynamicModule, Global, Module, OnModuleInit } from '@nestjs/common';
+import {DynamicModule, Global, Module, OnModuleInit} from '@nestjs/common';
 
-import { EarlyAccessController } from './controllers/early-access.controller';
-import { ConfigImpl } from './impl/config.impl';
-import { createEarlyAccessAsyncProviders } from './providers/early-access.async.providers';
-import { IllegalArgumentException } from './exception/illegal-argument.exception';
-import { EarlyAccessAsyncOptions, EarlyAccessModuleOptions } from './contracts/early-access-module-options.interface';
+import {EarlyAccessController} from './controllers/early-access.controller';
+import {ConfigImpl} from './impl/config.impl';
+import {createEarlyAccessAsyncProviders} from './providers/early-access.async.providers';
+import {IllegalArgumentException} from './exception/illegal-argument.exception';
+import {EarlyAccessAsyncOptions, EarlyAccessModuleOptions} from './contracts/early-access-module-options.interface';
 import {
-  createEarlyAccessProviders,
-  EarlyAccessRepositoryProvider,
-  EarlyAccessServiceProvider,
-  EarlyAccessViewLoaderProvider,
+    createEarlyAccessProviders,
+    EarlyAccessRepositoryProvider,
+    EarlyAccessServiceProvider,
+    EarlyAccessViewLoaderProvider,
 } from './providers/early-access.providers';
-import { ViewLoader } from './abstracts/view.loader';
-import { HttpAdapterHost } from '@nestjs/core';
+import {ViewLoader} from './abstracts/view.loader';
+import {HttpAdapterHost} from '@nestjs/core';
 
 
 @Global()
 @Module({
-  providers: [EarlyAccessViewLoaderProvider],
+    providers: [EarlyAccessViewLoaderProvider],
 })
 export class EarlyAccessModule implements OnModuleInit {
 
-  constructor(private readonly options: ConfigImpl,
-              private readonly controller: EarlyAccessController,
-              private readonly viewLoader: ViewLoader,
-              private readonly httpAdapterHost: HttpAdapterHost) {
-  }
-
-  static forRoot(options: EarlyAccessModuleOptions): DynamicModule {
-    if (options.template.index && !options.template.viewDir) {
-      throw new IllegalArgumentException('The view directory of the index file must be provided');
+    constructor(private readonly options: ConfigImpl,
+                private readonly controller: EarlyAccessController,
+                private readonly viewLoader: ViewLoader,
+                private readonly httpAdapterHost: HttpAdapterHost) {
     }
-    const providers = [...createEarlyAccessProviders(options),
-      ConfigImpl,
-      EarlyAccessRepositoryProvider,
-      EarlyAccessServiceProvider,
-      EarlyAccessViewLoaderProvider,
-      EarlyAccessController,
-    ];
-    return {
-      module: EarlyAccessModule,
-      providers: providers,
-    };
-  }
+
+    static forRoot(options: EarlyAccessModuleOptions): DynamicModule {
+        if (options.template.index && !options.template.viewDir) {
+            throw new IllegalArgumentException('The view directory of the index file must be provided');
+        }
+        const providers = [...createEarlyAccessProviders(options),
+            ConfigImpl,
+            EarlyAccessRepositoryProvider,
+            EarlyAccessServiceProvider,
+            EarlyAccessViewLoaderProvider,
+            EarlyAccessController,
+        ];
+        return {
+            module: EarlyAccessModule,
+            providers: providers,
+        };
+    }
 
 
-  static registerAsync(asyncOptions: EarlyAccessAsyncOptions): DynamicModule {
-    const providers = [...createEarlyAccessAsyncProviders(asyncOptions),
-      ConfigImpl,
-      EarlyAccessRepositoryProvider,
-      EarlyAccessServiceProvider,
-      EarlyAccessViewLoaderProvider,
-      EarlyAccessController];
+    static registerAsync(asyncOptions: EarlyAccessAsyncOptions): DynamicModule {
+        const providers = [...createEarlyAccessAsyncProviders(asyncOptions),
+            ConfigImpl,
+            EarlyAccessRepositoryProvider,
+            EarlyAccessServiceProvider,
+            EarlyAccessViewLoaderProvider,
+            EarlyAccessController];
 
-    return {
-      module: EarlyAccessModule,
-      providers,
-      imports: asyncOptions.imports,
-    };
-  }
+        return {
+            module: EarlyAccessModule,
+            providers,
+            imports: asyncOptions.imports,
+        };
+    }
 
-  onModuleInit(): any {
-    this.viewLoader.register(this.httpAdapterHost.httpAdapter, this.controller, this.options);
-  }
+    onModuleInit(): any {
+        this.viewLoader.register(this.httpAdapterHost.httpAdapter, this.controller, this.options);
+    }
 
 }
--- src/providers/early-access.async.providers.ts
+++ src/providers/early-access.async.providers.ts
@@ -1,6 +1,7 @@
-import { IllegalArgumentException } from '../exception/illegal-argument.exception';
-import { EarlyAccessAsyncOptions, EarlyAccessOptionsFactory } from '..';
-import { EARLY_ACCESS_OPTIONS } from '../constants/token.constants';
+import {IllegalArgumentException} from '../exception/illegal-argument.exception';
+import {EarlyAccessAsyncOptions, EarlyAccessOptionsFactory} from '..';
+import {EARLY_ACCESS_OPTIONS} from '../constants/token.constants';
+import * as fs from 'fs';
 
 export function createEarlyAccessAsyncProviders(options: EarlyAccessAsyncOptions) {
     if (options.useExisting || options.useFactory) {
@@ -28,12 +29,15 @@ function createEarlyAccessAsyncOptionsProviders(options: EarlyAccessAsyncOptions
     return {
         provide: EARLY_ACCESS_OPTIONS,
         useFactory: async (optionsFactory: EarlyAccessOptionsFactory) => {
-
             let earlyAccessOptions = await optionsFactory.createEarlyAccessOptions();
-            if (earlyAccessOptions.template.index && !earlyAccessOptions.template.viewDir) {
+            if (earlyAccessOptions.template?.index && !earlyAccessOptions.template?.viewDir) {
                 throw new IllegalArgumentException('The view directory of the index file must be provided');
             }
-            return earlyAccessOptions;
+            if (earlyAccessOptions.template?.viewDir && !fs.existsSync(earlyAccessOptions.template?.viewDir)) {
+                throw new IllegalArgumentException('Views dir provided does not exist');
+            }
+            if (earlyAccessOptions.template?.assetsDir && !fs.existsSync(earlyAccessOptions.template?.assetsDir))
+                throw new IllegalArgumentException('Assets dir provided does not exist');
         },
         inject: [options.useExisting || options.useClass],
 
