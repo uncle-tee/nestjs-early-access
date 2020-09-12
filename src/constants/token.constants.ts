8c6d8d35c644352a109578f0c2d35346c7e2e429
oluwatobi Adeenkan <tadenekan@dlabs.cloud>
2020-09-12 10:00:15 +0100

    Updated views dir

--- src/constants/token.constants.ts
+++ src/constants/token.constants.ts
@@ -1,7 +1,7 @@
 export const EARLY_ACCESS_OPTIONS = 'EARLY_ACCESS_OPTIONS';
 export const EARLY_ACCESS_URL = '/early-access';
-export const EARLY_ACCESS_ASSETS_DIR = `${__dirname}/../../../../../src/early-access/assets`;
-export const EARLY_ACCESS_VIEWS_DIR = `${__dirname}/../../../../../src/early-access/views`;
+export const EARLY_ACCESS_ASSETS_DIR = `${__dirname}/../../assets`;
+export const EARLY_ACCESS_VIEWS_DIR = `${__dirname}/../../views`;
 export const EARLY_ACCESS_INDEX = 'index';
 
 
