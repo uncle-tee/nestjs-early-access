60f09aa8a7138b00b1ef5a3c06427e5f3ef44227
oluwatobi Adeenkan <tadenekan@dlabs.cloud>
2020-09-14 08:21:32 +0100

    fix: Added a dummy view loader.

--- src/providers/early-access.providers.ts
+++ src/providers/early-access.providers.ts
@@ -1,50 +1,52 @@
-import { Provider } from '@nestjs/common';
-import { EarlyAccessServiceImpl } from '../impl/early-access-service.impl';
-import { EarlyAccessRepositoryImpl } from '../impl/early-access-repository.impl';
-import { HttpAdapterHost } from '@nestjs/core';
-import { EarlyAccessModuleOptions, EarlyAccessRepository } from '..';
-import { EARLY_ACCESS_OPTIONS } from '../constants/token.constants';
-import { ViewLoader } from '../abstracts/view.loader';
-import { ExpressLoaders } from '../loaders/express.loaders';
-import { EarlyAccessService } from '../contracts/early-access-service.interface';
+import {Provider} from '@nestjs/common';
+import {EarlyAccessServiceImpl} from '../impl/early-access-service.impl';
+import {EarlyAccessRepositoryImpl} from '../impl/early-access-repository.impl';
+import {HttpAdapterHost} from '@nestjs/core';
+import {EarlyAccessModuleOptions, EarlyAccessRepository} from '..';
+import {EARLY_ACCESS_OPTIONS} from '../constants/token.constants';
+import {ViewLoader} from '../abstracts/view.loader';
+import {ExpressLoaders} from '../loaders/express.loaders';
+import {EarlyAccessService} from '../contracts/early-access-service.interface';
+import {DummyLoaders} from "../loaders/dummy.loaders";
 
 
 export function createEarlyAccessProviders(options: EarlyAccessModuleOptions): Provider[] {
-  return [
-    {
-      provide: EARLY_ACCESS_OPTIONS,
-      useValue: options,
-    },
-  ];
+    return [
+        {
+            provide: EARLY_ACCESS_OPTIONS,
+            useValue: options,
+        },
+    ];
 }
 
 
 export const EarlyAccessServiceProvider = {
-  provide: EarlyAccessService,
-  useFactory: (repository: EarlyAccessRepository) => {
-    return new EarlyAccessServiceImpl(repository);
-  },
-  inject: [EarlyAccessRepository],
+    provide: EarlyAccessService,
+    useFactory: (repository: EarlyAccessRepository) => {
+        return new EarlyAccessServiceImpl(repository);
+    },
+    inject: [EarlyAccessRepository],
 
 };
 
 export const EarlyAccessRepositoryProvider = {
-  provide: EarlyAccessRepository,
-  useFactory: (options: EarlyAccessModuleOptions) => {
-    return options.repository ? options.repository : new EarlyAccessRepositoryImpl();
-  },
-  inject: [EARLY_ACCESS_OPTIONS],
+    provide: EarlyAccessRepository,
+    useFactory: (options: EarlyAccessModuleOptions) => {
+        return options.repository ? options.repository : new EarlyAccessRepositoryImpl();
+    },
+    inject: [EARLY_ACCESS_OPTIONS],
 };
 
 
 export const EarlyAccessViewLoaderProvider = {
-  provide: ViewLoader,
-  useFactory: (httpAdapterHost: HttpAdapterHost) => {
-    if (httpAdapterHost && httpAdapterHost.httpAdapter) {
-      return new ExpressLoaders();
-    }
-  },
-  inject: [HttpAdapterHost],
+    provide: ViewLoader,
+    useFactory: (httpAdapterHost: HttpAdapterHost) => {
+        if (httpAdapterHost && httpAdapterHost.httpAdapter) {
+            return new ExpressLoaders();
+        }
+        return new DummyLoaders();
+    },
+    inject: [HttpAdapterHost],
 };
 
 
